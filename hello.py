9e34cfd1a98059706293f69467748e508f248586
haitrth <haithanhtran00@gmail.com>
2025-04-13 15:59:39 +0700

    Variable and Expression

--- hello.py
+++ hello.py
@@ -6,3 +6,15 @@ print(23/7)
 print(2+3*6)
 print(2       +        2)
 print((5 - 1) * ((7 + 1) / (3 - 1)))
+
+print(2+2)
+
+spam = 'hello'
+print (spam + 'World')
+spam
+spam = "Goodbye"
+spam
+print (spam)
+greeting = "Hi"
+greeting =  "My name is Tran Thanh Hai"
+print(greeting)
