cc772cb47ac8c91715fa899846e29aa5f97b8fc6
haitrth <haithanhtran00@gmail.com>
2025-04-15 21:46:36 +0700

    Recall type

--- hello.py
+++ hello.py
@@ -6,9 +6,7 @@ print(23/7)
 print(2+3*6)
 print(2       +        2)
 print((5 - 1) * ((7 + 1) / (3 - 1)))
-
 print(2+2)
-
 spam = 'hello'
 print (spam + 'World')
 spam
@@ -25,3 +23,8 @@ print(type(42.0))
 print(type('42'))
 print("Test push code")
 print("Continue to leanrn code")
+print('Hi there, ' +  + '! Welcome to Python!')
+print('Hi there, ')
+hello="THanh hai"
+print('Hi there' + hello + 'welcome to Python')
+print('Hi there' + hello + 'welcome you to my home')
